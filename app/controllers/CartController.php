d7280e4aeedfba1959b87a64e9f5d505e5ad9c0d
Nata Hoshovska <83783581+natrixcode@users.noreply.github.com>
2023-04-22 13:54:55 +0300

    Completed lesson 13 (Part 2)

---
+++ app/controllers/CartController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use app\models\Cart;
+use wfm\App;
+
+/** @property Cart $model */
+class CartController extends AppController
+{
+
+    public function addAction(): bool
+    {
+        $lang = App::$app->getProperty('language');
+        $id = get('id');
+        $qty = get('qty');
+
+        if (!$id) {
+            return false;
+        }
+
+        $product = $this->model->get_product($id, $lang);
+        debug($product, 1);
+    }
+}
+?>
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,29 @@
 $(function() {
 
+	//cart
+
+	$('.add-to-cart').on('click', function (e) {
+		e.preventDefault();
+		const id = $(this).data('id');
+		const qty = $('#input-quantity').val() ? $('#input-quantity').val() : 1;
+		const $this = $(this);
+
+		$.ajax({
+			url: 'cart/add',
+			type: 'GET',
+			data: {id: id, qty: qty},
+			success: function (res) {
+				console.log(res);
+				// showCart(res);
+				// $this.find('i').removeClass('fa-shopping-cart').addClass('fa-luggage-cart');
+			},
+			error: function () {
+				alert('Error!');
+			}
+		});
+	});
+	//
+
 	$('.open-search').click(function(e) {
 		e.preventDefault();
 		$('#search').addClass('active');
