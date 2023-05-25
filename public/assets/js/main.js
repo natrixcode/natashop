a12718e8243fd9e58fdecdf0d2f6174d1ee202f2
Nata Hoshovska <83783581+natrixcode@users.noreply.github.com>
2023-05-25 19:58:41 +0300

    Completed lesson 27 (Part 2)

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -102,6 +102,25 @@ $(function() {
 	// CART
 
 
+	$('.product-card').on('click', '.add-to-wishlist', function (e) {
+		e.preventDefault();
+		const id = $(this).data('id');
+		const $this = $(this);
+		$.ajax({
+			url: 'wishlist/add',
+			type: 'GET',
+			data: {id: id},
+			success: function (res) {
+				res = JSON.parse(res);
+				console.log(res);
+			},
+			error: function () {
+				alert('Error!');
+			}
+		});
+	});
+
+
 	$('.open-search').click(function(e) {
 		e.preventDefault();
 		$('#search').addClass('active');
