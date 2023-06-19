3196763a8637a91052a6755c441c20279e0e9838
Nata Hoshovska <83783581+natrixcode@users.noreply.github.com>
2023-06-19 19:39:12 +0300

    Completed lesson 42 (Part 2)

--- app/models/User.php
+++ app/models/User.php
@@ -79,4 +79,9 @@ class User extends AppModel
     {
         return R::getAll("SELECT * FROM orders WHERE user_id = ? ORDER BY id DESC LIMIT $start, $perpage", [$user_id]);
     }
+
+    public function get_user_order($id): array 
+    {
+        return R::getAll("SELECT o.*, op.* FROM orders o JOIN order_product op on o.id = op.order_id WHERE o.id = ?", [$id]);
+    }
 }
