56237f2f9d048d3e0782fc66a967dd3d123c3903
Nata Hoshovska <83783581+natrixcode@users.noreply.github.com>
2023-06-29 17:14:26 +0300

    Completed lesson 2 (Part 3)

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -3,12 +3,15 @@
 
 namespace app\controllers\admin;
 
+
 class MainController extends AppController
 {
 
     public function indexAction()
     {
-
+        $title = 'Home page';
+        $this->setMeta('Admin | Home page');
+        $this->set(compact('title'));
     }
 
 }
