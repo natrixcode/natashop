30d034c09db5af088d9515ea8baca0d0adf6da04
Nata Hoshovska <83783581+natrixcode@users.noreply.github.com>
2023-07-03 20:17:37 +0300

    Completed lesson 4 (Part 3)

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -3,15 +3,21 @@
 
 namespace app\controllers\admin;
 
+use RedBeanPHP\R;
 
 class MainController extends AppController
 {
 
     public function indexAction()
     {
+        $orders = R::count('orders');
+        $new_orders = R::count('orders', 'status = 0');
+        $users = R::count('user');
+        $products = R::count('product');
+
         $title = 'Home page';
         $this->setMeta('Admin | Home page');
-        $this->set(compact('title'));
+        $this->set(compact('title', 'orders', 'new_orders', 'users', 'products'));
     }
 
 }
