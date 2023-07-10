9b019916db3f82c439e3a808d303b98c06ec3a5f
Nata Hoshovska <83783581+natrixcode@users.noreply.github.com>
2023-07-10 13:14:56 +0300

    Completed lesson 6 (Part 3)

--- app/controllers/admin/CategoryController.php
+++ app/controllers/admin/CategoryController.php
@@ -3,6 +3,7 @@
 
 namespace app\controllers\admin;
 
+use RedBeanPHP\R;
 
 class CategoryController extends AppController
 {
@@ -12,6 +13,28 @@ class CategoryController extends AppController
         $title = 'Категорії';
         $this->setMeta("Адмінка :: {$title}");
         $this->set(compact('title'));
+    }
+
+    public function deleteAction()
+    {
+        $id = get('id');
+        $errors = '';
+        $children = R::count('category', 'parent_id = ?', [$id]);
+        $products = R::count('product', 'category_id = ?', [$id]);
+        if ($children) {
+            $errors .= 'Помилка! В категорії є вкладені категорії<br>';
+        }
+        if ($products) {
+            $errors .= 'Помилка! В категорії є товари<br>';
+        }
+        if ($errors) {
+            $_SESSION['errors'] = $errors;
+        } else {
+            R::exec("DELETE FROM category WHERE id = ?", [$id]);
+            R::exec("DELETE FROM category_description WHERE category_id = ?", [$id]);
+            $_SESSION['success'] = 'Категорія видалена';
+        }
+        redirect();
     }
 
 }
